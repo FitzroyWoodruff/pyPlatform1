efa649c8b25d6897d19d52c60281c7716118e0f7
Fitzroy Woodruff <fitzroywoodruff@gmail.com>
2023-02-19 13:43:47 -0500

    Added tiles for background

--- main.py
+++ main.py
@@ -38,11 +38,38 @@ VELOCITY = 5
 #create the pygame window
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+# generate the background
+def get_background(name):
+    #load the images in background folder
+    image = pygame.image.load(join("assets", "Backgrounds", name))
+    # get the width and height of the tiles while ignoring the x and y
+    _, _, width, height = image.get_rect()
+    # create an empty list of tiles
+    tiles = []
+    
+    # determine the number of tiles needed for the width and height
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            # pos of the tiles
+            pos = (i * width, j * height)
+            tiles.append(pos)
+            
+    return tiles, image
+
+# define the draw function
+def draw(window, background, bg_image):
+    for tile in background:
+        # add image to window
+        window.blit(bg_image, tile)
+    
+    pygame.display.update()
 
 # main function
 def main(window):
     # establish the clock
     clock = pygame.time.Clock()
+    # set the level background
+    background, bg_image = get_background("Pyramid.jpg")
     
     # Running boolean
     run = True
@@ -59,6 +86,7 @@ def main(window):
                 run = False
                 break
             
+        draw(window, background, bg_image)
     pygame.quit()
     quit()
 
