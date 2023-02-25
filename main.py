d680e8eb816f6041b8982b2601272531f8428e3a
Fitzroy Woodruff <fitzroywoodruff@gmail.com>
2023-02-24 22:59:49 -0500

    added sprite loading

--- main.py
+++ main.py
@@ -38,11 +38,44 @@ VELOCITY = 5
 #create the pygame window
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+# image flipping
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+def load_sprite_sheets(dir1, dir2, width, height, direction= False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+    
+    all_sprites = {}
+    
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+        
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+            
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "") ] = sprites
+            
+    return all_sprites
+            
+            
+
 # define the player class
 class Player(pygame.sprite.Sprite):
     RED = (255, 0, 0)
     # set Gravity
     GRAVITY = 1
+    
+    # load sprites
+    SPRITES = load_sprite_sheets("Players", "NinjaFrog", 32, 32, True)
     
     #object initialization
     def __init__(self, x, y, width, height):
@@ -72,13 +105,16 @@ class Player(pygame.sprite.Sprite):
             self.animation_count = 0
             
     def update(self, fps):
-        self.y_velocity += min(1, (self.fall_count / fps) * self.GRAVITY)
+        # self.y_velocity += min(1, (self.fall_count / fps) * self.GRAVITY)
+        
         self.move(self.x_velocity, self.y_velocity)
         
         self.fall_count += 1
         
     def draw(self, window):
-        pygame.draw.rect(window, self.RED, self.rect)
+        # pygame.draw.rect(window, self.RED, self.rect)
+        self.sprite = self.SPRITES["idle_" + self.direction][0]
+        window.blit(self.sprite, (self.rect.x, self.rect.y))
 
 # generate the background
 def get_background(name):
